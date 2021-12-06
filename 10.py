b6c07d9517ecc4c3d01aff8de62cf341a43d557f
Mack Goodstein <mack@vpn68-205.vpnclient.ucar.edu>
2021-12-05 21:31:10 -0700

    Uploading day 5

---
+++ 10.py
@@ -0,0 +1,43 @@
+import clipboard
+import numpy as np
+
+input = clipboard.paste().splitlines()
+
+def getPoints(line):
+	endpoints = line.split(" -> ")
+	endpoint1 = endpoints[0].split(",")
+	endpoint2 = endpoints[1].split(",")
+	x1 = int(endpoint1[0])
+	y1 = int(endpoint1[1])
+	x2 = int(endpoint2[0])
+	y2 = int(endpoint2[1])
+	points = []
+	if x1 == x2:
+		for i in range(y1, y2+1):
+			points.append([i, x1])
+		for i in range(y2, y1+1):
+			points.append([i, x1])
+
+	elif y1 == y2:
+		for i in range(x1, x2+1):
+			points.append([y1, i])
+		for i in range(x2, x1+1):
+			points.append([y1, i])
+	else:
+		xadd = 1 if x1 < x2 else -1
+		yadd = 1 if y1 < y2 else -1
+		for i in range((y2-y1)*yadd + 1):
+			points.append([y1 + i*yadd, x1 + i*xadd])
+	return points
+
+coordinates = np.zeros([1000,1000])
+
+for line in input:
+	points = getPoints(line)
+	# print(points)
+	if points:
+		for point in points:
+			coordinates[point[0],point[1]] += 1
+
+print(np.count_nonzero(coordinates > 1))
+# print(coordinates)
